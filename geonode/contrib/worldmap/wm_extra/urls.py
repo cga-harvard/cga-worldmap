b6578f02b151c92232f9fff8afd311fd9f9088bc
Niran814804102 <814804102@qq.com>
2018-09-25 12:04:30 +0800

    Convert django-geoexplorer to django-geoexplorer-worldmap for layer details page

--- geonode/contrib/worldmap/wm_extra/urls.py
+++ geonode/contrib/worldmap/wm_extra/urls.py
@@ -8,7 +8,7 @@ from .views import (ajax_snapshot_history, layer_searchable_fields,
                     ajax_layer_edit_style_check, upload_layer,
                     ajax_increment_layer_stats, add_layer_wm,
                     new_map_wm, new_map_json_wm, map_view_wm, map_json_wm,
-                    map_detail_wm, map_view_wm_mobile, add_endpoint, printmap, )
+                    map_detail_wm, map_view_wm_mobile, add_endpoint, printmap, layer_detail_wm )
 
 from tastypie.api import Api
 from .api.resources import (LayerResource, TagResource, TopicCategoryResource,
@@ -68,6 +68,7 @@ urlpatterns = [
         r'^data/(?P<layername>[^/]*)/ajax_layer_update/?$',
         ajax_layer_update,
         name='ajax_layer_update'),
+    url(r'^layers/(?P<layername>[^/]*)$', layer_detail_wm, name="layer_detail_wm"),
     # url(r'^data/create_pg_layer', create_pg_layer, name='create_pg_layer'),
     url(r'^data/upload', upload_layer, name='data_upload'),
     url(r'^data/layerstats', ajax_increment_layer_stats, name='layer_stats'),
